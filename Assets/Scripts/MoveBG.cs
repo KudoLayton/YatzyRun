7c12cf9e5aa0d940f4a7474df7bf5e2effd379d7
KudoLayton <lhslhg@gmail.com>
2020-07-26 14:42:28 +0900

    feat: add parallex background feature

---
+++ Assets/Scripts/MoveBG.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveBG : MonoBehaviour
+{
+    MeshRenderer renderer;
+    [SerializeField]
+    [Range(0.0f, 1.0f)]
+    float speed;
+    void Start()
+    {
+        renderer = GetComponent<MeshRenderer>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        renderer.material.SetTextureOffset("_MainTex",
+            renderer.material.GetTextureOffset("_MainTex") + new Vector2(speed, 0) * Time.deltaTime);
+    }
+}
